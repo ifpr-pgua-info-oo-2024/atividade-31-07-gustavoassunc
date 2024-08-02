a9e326ba03dd39d581619f770e82d4220f57875c
gustavoassunc <gustavoassunc.andrade@gmailcom>
2024-08-02 19:03:37 -0300

    tareda 01

--- Main.java
+++ Main.java
@@ -2,9 +2,26 @@ public class Main {
     
     public static void main(String[] args) {
 
-       
-        System.out.println("Faça sua atividade");
+       Academia a = new Academia("World", "Rua Tókio, 472", "41 9999-9999");
  
+       System.out.println(a.getNome()+" "+a.getEndereco()+" "+a.getTelefone());
+       a.setNome("Bio Aventura");
+       a.setEndereco("Av Bento Munhoz da Rocha Neto, 12");
+       a.setTelefone("41 3333-3333");
+       System.out.println(a.getNome()+" "+a.getEndereco()+" "+a.getTelefone());
+
+       Academia b = new Academia("caopi", "lgbhfd, 77", "41 3434-5677");
+
+       System.out.println("     OBJETO 2 ");
+       System.out.println(b.getNome()+" "+ b.getEndereco()+" "+b.getTelefone());
+
+       b.setNome("dominio");
+       b.setEndereco("Av Roque, 32");
+       b.setTelefone("41 2193-9856");
+       
+       System.out.println(b.getNome()+" "+b.getEndereco()+" "+b.getTelefone());
+
+
     }
  
 }
